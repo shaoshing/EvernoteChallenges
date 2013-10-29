201c0c4ab9a98073af4d8a335353c6f3be925de2
Shaoshing <shaoshing@me.com>
2013-10-29 19:05:30 -0400

    Rewrite the ruby circular buffer with class.

--- circular-buffer.rb
+++ circular-buffer.rb
@@ -1,49 +1,63 @@
-buffer = []
-buffer_max_size = gets.to_i
-buffer_size = 0
-buffer_is_empty = true
-start_of_buffer = 0
-end_of_buffer = 0
+class CircularBuffer
+  def initialize(buffer_size)
+    @buffer = []
+    @buffer_max_size = buffer_size
+    @buffer_size = 0
+    @start_of_buffer = 0
+    @end_of_buffer = 0
+  end
 
+  def add( items )
+    items.each do |item|
+      if @buffer_size == 0
+        @start_of_buffer = 0
+        @end_of_buffer = 0
+        @buffer_size = 1
+      else
+        @end_of_buffer = (@end_of_buffer+1)%@buffer_max_size
+        @buffer_size += 1
+        if @buffer_size > @buffer_max_size
+          @buffer_size = @buffer_max_size
+          @start_of_buffer = (@start_of_buffer+1)%@buffer_max_size
+        end
+      end
+
+      @buffer[@end_of_buffer] = item
+    end
+  end
+
+  def remove( removalCount )
+    return if @buffer_size == 0
+    if @buffer_size == removalCount
+      @buffer_size = 0
+    else
+      @start_of_buffer = (@start_of_buffer+removalCount)%@buffer_max_size
+      @buffer_size -= removalCount
+    end
+  end
+
+  def list
+    @buffer_size.times do |n|
+      index = (@start_of_buffer+n)%@buffer_max_size
+      puts @buffer[index]
+    end
+  end
+end
+
+buffer = CircularBuffer.new(gets.to_i)
 command = ""
 while command = gets do
   case command[0]
   when "A"
-    addCount = command.split(" ")[1].to_i
-    addCount.times do
-      if buffer_is_empty
-        start_of_buffer = 0
-        end_of_buffer = 0
-        buffer_is_empty = false
-        buffer_size = 1
-      else
-        end_of_buffer = (end_of_buffer+1)%buffer_max_size
-        buffer_size += 1
-        if buffer_size > buffer_max_size
-          buffer_size = buffer_max_size
-          start_of_buffer = (start_of_buffer+1)%buffer_max_size
-        end
-      end
-
-      item = gets
-      buffer[end_of_buffer] = item
+    items = []
+    command.split(" ")[1].to_i.times do
+      items << gets
     end
+    buffer.add(items)
   when "R"
-    next if buffer_is_empty
-    removalCount = command.split(" ")[1].to_i
-    if buffer_size == removalCount
-      buffer_is_empty = true
-      buffer_size = 0
-    else
-      start_of_buffer = (start_of_buffer+removalCount)%buffer_max_size
-      buffer_size -= removalCount
-    end
+    buffer.remove(command.split(" ")[1].to_i)
   when "L"
-    next if buffer_is_empty
-    buffer_size.times do |n|
-      index = (start_of_buffer+n)%buffer_max_size
-      puts buffer[index]
-    end
+    buffer.list
   when "Q"
     exit
   end
